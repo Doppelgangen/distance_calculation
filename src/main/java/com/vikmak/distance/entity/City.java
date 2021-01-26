aa7e419dab563f02e95e0b2b7a6525a556a21ab6
Viktor Makarov <doppelgangen666@ya.ru>
2021-01-26 16:06:02 +0400

    first commit

---
+++ src/main/java/com/vikmak/distance/entity/City.java
@@ -0,0 +1,13 @@
+package com.vikmak.distance.entity;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Viktor Makarov
+ */
+
+@XmlRootElement
+@Entity
+public class City {
+}
