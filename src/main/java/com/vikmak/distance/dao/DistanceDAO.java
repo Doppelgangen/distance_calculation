c9bff53195682e0597afe5108acb2b97e36b9201
Viktor Makarov <doppelgangen666@ya.ru>
2021-01-31 20:16:04 +0400

    just a little more coding...

--- src/main/java/com/vikmak/distance/dao/DistanceDAO.java
+++ src/main/java/com/vikmak/distance/dao/DistanceDAO.java
@@ -1,11 +1,14 @@
 package com.vikmak.distance.dao;
 
+import com.vikmak.distance.entity.City;
 import com.vikmak.distance.services.CityService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Viktor Makarov
@@ -19,11 +22,33 @@ public class DistanceDAO {
     @POST
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response getMethod(@FormParam("CalculationType")String string){
+    public Response getMethod(
+            @FormParam("calculationType")String calculationType,
+            @FormParam("cityFrom") String cityFrom,
+            @FormParam("cityTo") String cityTo){
 
+        List<String> origins = Arrays.asList(cityFrom.split("\\W+"));
+        List<String> destinations = Arrays.asList(cityTo.split("\\W+"));
+/*
+        switch (calculationType) {
+            case "Crowflight":
+                showCroflight(List<String> origins, List<String> destinations);
+                break;
+            case "Distance Matrix":
+                showDistanceMatrix();
+                break;
+            case "All":
+                showAll();
+                break;
+            default:
+                Response.noContent().build();
+        }
+*/
         return Response.status(200)
-                .entity("addUser is called, name : " + string)
+                .entity("addUser is called, name : " + calculationType + cityFrom + cityTo)
                 .build();
     }
+
+
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,14 +6,21 @@
 <h4>
     <a href="/rest/cities">Display list of all cities</a>
 </h4>
-<div/>
+<br/>
 <form action="/rest/distance" method="post">
     <h4>Choose measurements</h4>
-    <select size="3" name="CalculationType">
-        <option>Crowflight</option>
+    <select size="3" name="calculationType">
+        <option selected>Crowflight</option>
         <option>Distance Matrix</option>
         <option>All</option>
     </select>
+    <br/>
+    <p>
+    City from : <input type="text" name="cityFrom"/>
+    </p>
+    <p>
+    City to : <input type="text" name="cityTo"/>
+    </p>
     <button type="submit" >Choose</button>
 </form>
 </body>
